e0f1dd6402a90a0c2de7768c73a7f5651cc40a53
Fran Rodriguez <f.x.rodriguez@hotmail.com>
2016-03-19 13:41:40 +0000

    Removed compilerconfig.json and added package.json and gulpfile with sass tasks

---
+++ franr/franr/gulpfile.js
@@ -0,0 +1,22 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('./scss/style.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('.'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./scss/*.scss', ['sass']);
+});
+
+//gulp.task('default', function () {
+//    
+//});
